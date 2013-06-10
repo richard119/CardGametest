10119148b40ccec47640868bcc9b3d4077f70c95
richard119 <richard119@gmail.com>
2013-06-10 18:32:56 +0800

    Initial Commit

---
+++ CardGametest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CardGametest
+//
+//  Created by Richard on 13-6-10.
+//  Copyright (c) 2013年 Richard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CardGametest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CardGametest
+//
+//  Created by Richard on 13-6-10.
+//  Copyright (c) 2013年 Richard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
